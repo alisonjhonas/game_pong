2b44d97926ee2c5b2f977a64f8ab9d3e94e2c3b9
Alison <alison.sousa@info.ufrn.br>
2019-10-25 18:03:19 -0300

    Configuração inicial do jogo

---
+++ pong/src/pong/Entity.java
@@ -0,0 +1,8 @@
+package pong;
+
+public interface Entity {
+	
+	public void tick();
+	
+	public void render();
+}
---
+++ pong/src/pong/Player.java
@@ -0,0 +1,17 @@
+package pong;
+
+public class Player implements Entity{
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
